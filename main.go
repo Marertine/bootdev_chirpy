9ba934d52c5e6f75368c21c0ee9a993a5aad516e
Marertine <rhodrirhamantus@gmail.com>
2026-01-09 08:58:34 +1100

    Ch2-2: Stateful handlers, complete

--- main.go
+++ main.go
@@ -4,14 +4,36 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	fmt.Println("Boot.Dev/Twitter Clone")
 
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
+	filepathRoot := "."
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(
+		http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))),
+	))
+
 	mux.HandleFunc("/healthz", healthzHandler)
+
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		metricsHandler(w, r, &apiCfg)
+	})
+
+	mux.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
+		resetHandler(w, r, &apiCfg)
+	})
 
 	server := http.Server{
 		Addr:    ":8080",
@@ -28,8 +50,28 @@ func main() {
 
 }
 
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+func metricsHandler(w http.ResponseWriter, r *http.Request, cfg *apiConfig) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func resetHandler(w http.ResponseWriter, r *http.Request, cfg *apiConfig) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Metrics reset"))
 }
